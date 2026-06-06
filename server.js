48f71fe7ffed3b5b01da0b227a256213f024ad32
RAVARI Team <everyinch360@gmail.com>
2026-06-07 02:45:24 +0530

    Clean up root server.js entry point

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // Root server.js - Entry point for Hostinger deployment
 // This file allows deployment with Root directory "./"
-require('dotenv').config();
+
+// Load backend server (which handles everything including app.listen())
 require('./backend/server.js');
