37b64de4298881638913d43a8cb5df40756f40ce
Ravari <ravari.store@gmail.com>
2026-09-04 18:12:41 +0530

    Refactor User.js for code formatting
    Removed unnecessary whitespace and ensured consistent formatting.

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-
 const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
@@ -57,11 +56,9 @@ const userSchema = new mongoose.Schema({
 }, {
   timestamps: true
 });
-
 // Hash password before saving
 userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
-
   try {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
@@ -70,17 +67,14 @@ userSchema.pre('save', async function(next) {
     next(error);
   }
 });
-
 // Method to compare passwords
 userSchema.methods.comparePassword = async function(candidatePassword) {
   return await bcrypt.compare(candidatePassword, this.password);
 };
-
 // Remove password from JSON
 userSchema.methods.toJSON = function() {
   const user = this.toObject();
   delete user.password;
   return user;
 };
-
 module.exports = mongoose.model('User', userSchema);
